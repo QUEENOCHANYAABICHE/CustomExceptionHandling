5970bdeab512accae7c71e319345758d68950066
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-07 18:35:32 +0100

    exceptions

---
+++ src/main/java/com/example/customexceptionhandling/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.example.customexceptionhandling.exceptions;
+
+public class ApiExceptionHandler {
+}
---
+++ src/test/java/com/example/customexceptionhandling/CustomExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
