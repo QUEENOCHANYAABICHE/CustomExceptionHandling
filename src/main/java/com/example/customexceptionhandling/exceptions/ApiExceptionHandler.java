176790734dc306b4c6c3abb5d46f14916cf4cd75
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-07 20:29:32 +0100

    exceptions

--- src/main/java/com/example/customexceptionhandling/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/example/customexceptionhandling/exceptions/ApiExceptionHandler.java
@@ -1,12 +1,24 @@
 package com.example.customexceptionhandling.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class ApiExceptionHandler {
 
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
         //1. Create payload containing exception details
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+         ApiException apiException = new ApiException(
+                e.getMessage(),
+                e,
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
         //2. Return responseEntity
+        return new ResponseEntity<>(apiException, badRequest);
     }
 
 }
