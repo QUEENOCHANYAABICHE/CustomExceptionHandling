e9319ac85fd72277926572aef3fe7cad37421de4
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-07 19:58:44 +0100

    exceptions

--- src/main/java/com/example/customexceptionhandling/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/example/customexceptionhandling/exceptions/ApiExceptionHandler.java
@@ -1,4 +1,12 @@
 package com.example.customexceptionhandling.exceptions;
 
+import org.springframework.http.ResponseEntity;
+
 public class ApiExceptionHandler {
+
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+        //1. Create payload containing exception details
+        //2. Return responseEntity
+    }
+
 }
