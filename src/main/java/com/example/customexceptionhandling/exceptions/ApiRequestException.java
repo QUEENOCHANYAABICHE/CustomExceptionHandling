b6d3224a1a7cceeb00d4dae5e273833b33eaf40c
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-07 18:53:49 +0100

    exceptions

---
+++ src/main/java/com/example/customexceptionhandling/exceptions/ApiRequestException.java
@@ -0,0 +1,14 @@
+package com.example.customexceptionhandling.exceptions;
+
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
